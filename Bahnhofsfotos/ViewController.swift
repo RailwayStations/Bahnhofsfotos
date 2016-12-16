5e6af9598cd41dd0059b0b719ac0e1c614b6f508
Haitec <me@mrhaitec.de>
2016-12-16 22:13:47 +0100

    Initial commit

---
+++ Bahnhofsfotos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Bahnhofsfotos
+//
+//  Created by Miguel Dönicke on 16.12.16.
+//  Copyright © 2016 MrHaitec. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
