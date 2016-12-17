c85bcc8580ac56e3bbb1013e24b92652952cf4b7
Haitec <me@mrhaitec.de>
2016-12-17 02:10:58 +0100

    Bahnhöfe filtern

--- Bahnhofsfotos/ViewController.swift
+++ Bahnhofsfotos/ViewController.swift
@@ -12,15 +12,32 @@ import AlamofireSwiftyJSON
 
 class ViewController: UITableViewController {
 
+    let searchController = UISearchController(searchResultsController: nil)
+
     var bahnhoefe: [Bahnhof]?
+    var gefilterteBahnhoefe: [Bahnhof]?
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        searchController.searchResultsUpdater = self
+        searchController.dimsBackgroundDuringPresentation = false
+        definesPresentationContext = true
+        tableView.tableHeaderView = searchController.searchBar
+
         bahnhoefe?.removeAll()
         bahnhoefe = []
 
+        showBahnhoefeOnheFoto()
+    }
+
+    // Bahnhöfe auslesen und anzeigen
+    func showBahnhoefeOnheFoto() {
+        UIApplication.shared.isNetworkActivityIndicatorVisible = true
+
         Alamofire.request(Constants.BAHNHOEFE_OHNE_PHOTO_URL).responseSwiftyJSON { response in
+            UIApplication.shared.isNetworkActivityIndicatorVisible = false
+
             guard let json = response.result.value?.array else { return }
 
             for bh in json {
@@ -30,29 +47,46 @@ class ViewController: UITableViewController {
             }
 
             self.sortBahnhoefe()
-            self.showBahnhoefeOhneFoto()
+            self.tableView.reloadData()
         }
     }
 
+    // Bahnhöfe sortieren
     func sortBahnhoefe() {
         bahnhoefe?.sort(by: { (bahnhof1, bahnhof2) -> Bool in
             bahnhof1.title < bahnhof2.title
         })
     }
 
-    func showBahnhoefeOhneFoto() {
-        self.tableView.reloadData()
+    // Bahnhöfe filtern
+    func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+        gefilterteBahnhoefe = bahnhoefe?.filter { bahnhof in
+            return bahnhof.title.lowercased().contains(searchText.lowercased())
+        }
+
+        tableView.reloadData()
     }
 
+    // TableView: Anzahl der anzeigbaren Bahnhöfe
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if searchController.isActive && searchController.searchBar.text != "" {
+            return gefilterteBahnhoefe?.count ?? 0
+        }
         return bahnhoefe?.count ?? 0
     }
 
+    // TableView: Zelle für spezifischen Bahnhof
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") ?? UITableViewCell()
 
-        if let bahnhof = bahnhoefe?[indexPath.row] {
-            cell.textLabel?.text = bahnhof.title
+        var bahnhof: Bahnhof?
+        if searchController.isActive && searchController.searchBar.text != "" {
+            bahnhof = gefilterteBahnhoefe?[indexPath.row]
+        } else {
+            bahnhof = bahnhoefe?[indexPath.row]
+        }
+        if bahnhof != nil {
+            cell.textLabel?.text = bahnhof?.title
         }
 
         return cell
@@ -60,3 +94,14 @@ class ViewController: UITableViewController {
 
 }
 
+// MARK: - UISearchResultsUpdating
+extension ViewController: UISearchResultsUpdating {
+
+    public func updateSearchResults(for searchController: UISearchController) {
+        guard let query = searchController.searchBar.text else {
+            return
+        }
+        filterContentForSearchText(query)
+    }
+
+}
