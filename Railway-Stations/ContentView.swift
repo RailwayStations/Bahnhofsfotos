1f82f8fbfd7be31111beb0f84cc95b6608fab41b
Miguel Dönicke <miguel@doenicke.io>
2022-04-29 22:58:13 +0200

    initializing SwiftUI app

---
+++ Railway-Stations/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Railway-Stations
+//
+//  Created by Miguel Dönicke on 29.04.22.
+//  Copyright © 2022 Railway-Stations. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Railway-Stations/RailwayStationsApp.swift
@@ -0,0 +1,18 @@
+//
+//  RailwayStationsApp.swift
+//  Railway-Stations
+//
+//  Created by Miguel Dönicke on 29.04.22.
+//  Copyright © 2022 Railway-Stations. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct RailwayStationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
